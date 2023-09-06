968edeccbfca0fd3ae28787cfb0abc314abef419
luanrdias23 <luan.rdias@gmail.com>
2023-09-06 15:04:27 -0300

    Passando valores pela URL

---
+++ src/listaProdutos.js
@@ -0,0 +1,24 @@
+export const listaProdutos = [
+    {
+        id:1,
+        nome:'Iphone 11',
+        preco:3000
+    },
+    { 
+        id: 2, 
+        nome:'Iphone 12',
+        preco: 3800
+        
+    },
+    {
+        id:3,
+        nome:'Iphone 13',
+        preco: 4000
+        
+    },
+    {
+        id:4,
+        nome:'Iphone 14',
+        preco: 5000
+    },
+]
--- src/routes/Produtos/index.jsx
+++ src/routes/Produtos/index.jsx
@@ -1,9 +1,17 @@
-
-
+import { Link } from "react-router-dom"
+import { listaProdutos } from "../../listaProdutos"
 export default function Produtos(){
     return (
         <main>
             <h1>Produtos</h1>
+            {listaProdutos.map(prod => (
+                <div key={prod.id}>
+                    <Link to ={`./produtos/editar/${prod.id}`}>
+                        Editar o produto: {prod.nome}
+                    </Link>
+                </div>
+
+            ))}
         </main>
     )
 }
